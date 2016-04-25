49bed92157c9ff8a1d3e5cbd19a7bad25c778d9e
ZeCaptain <alanpatrickmeagher@gmail.com>
2016-04-25 04:20:54 +0100

    First commit

---
+++ AlphaTest/src/alphatest/AlphaTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphatest;
+
+/**
+ *
+ * @author ZeCaptain
+ */
+public class AlphaTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Your going to fail");
+    }
+    
+}
